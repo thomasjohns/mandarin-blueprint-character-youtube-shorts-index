38c61328727c4dde1caf498dd533e52b63279a79
Thomas Johnson <tej5@uw.edu>
2023-10-22 01:35:52 -0700

    Make script generate full README.md file (including title header)

--- generate_index_from_toml_data.py
+++ generate_index_from_toml_data.py
@@ -5,6 +5,8 @@ with open('char_to_blueprint_shorts.toml', 'rb') as fp:
     char_to_blueprint_shorts = tomllib.load(fp)
 
 
+print('# Mandarin Blueprint character Youtube shorts index')
+print()
 for char, shorts in char_to_blueprint_shorts.items():
     print(f'- {char}')
     for short in shorts:
